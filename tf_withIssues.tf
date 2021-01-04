ead7e95a6caadb053dfc977aeaa0d20144f6c131
nibansal <74949344+nibansal@users.noreply.github.com>
2021-01-04 19:25:50 +0530

    Update tf_withIssues.tf

--- tf_withIssues.tf
+++ tf_withIssues.tf
@@ -2,12 +2,12 @@ terraform {
     required_version = ">= 0.12.0"
   }
   
-  provider "aw" {
+  provider "aws" {
     region = "us-east-1"
   }
   
   resource "aws_s3_bucket" "foo-test" {
-    bucket = "my-tf-log-bucket-test"
+    buckets = "my-tf-log-bucket-test"
     acl = "public-read-write"
   }
 
